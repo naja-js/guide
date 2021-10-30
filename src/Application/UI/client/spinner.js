542a9bab05a96a6b46f792a7364346900b1da0ad
Jiří Pudil <me@jiripudil.cz>
2021-10-30 18:44:33 +0200

    Make the spinner localized

--- src/Application/UI/client/spinner.js
+++ src/Application/UI/client/spinner.js
@@ -4,8 +4,13 @@ export const spinnerExtension = {
 	initialize(naja) {
 		onDomReady(() => {
 			const mainContent = document.querySelector('.mainContent');
-			naja.addEventListener('start', () => mainContent.classList.add('spinner'));
-			naja.addEventListener('complete', () => mainContent.classList.remove('spinner'));
+
+			naja.uiHandler.addEventListener('interaction', (event) => {
+				event.detail.options.spinnerTarget = event.detail.element.closest('[data-spinner]');
+			});
+
+			naja.addEventListener('start', (event) => (event.detail.options.spinnerTarget ?? mainContent).classList.add('spinner'));
+			naja.addEventListener('complete', (event) => (event.detail.options.spinnerTarget ?? mainContent).classList.remove('spinner'));
 		});
 	}
 }
